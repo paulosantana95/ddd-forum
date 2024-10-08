0069624839508ee741d57c247115a520c6536fec
Paulo Santana <paulosantana.sa@gmail.com>
2024-10-08 00:36:27 -0300

    feat: implement fetch question answers use case and add findManyByQuestionId method to AnswersRepository

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,29 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answer: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answer = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    )
+
+    return {
+      answer,
+    }
+  }
+}
